ce151c4cc2e7d71b2200568d69595df63ba07d4e
FG <gomezfv95@gmail.com>
2024-05-22 16:39:30 -0300

    first commit

---
+++ src/main/java/AlkemyWallet/AlkemyWallet/AlkemyWalletApplication.java
@@ -0,0 +1,13 @@
+package AlkemyWallet.AlkemyWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkemyWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkemyWalletApplication.class, args);
+	}
+
+}
