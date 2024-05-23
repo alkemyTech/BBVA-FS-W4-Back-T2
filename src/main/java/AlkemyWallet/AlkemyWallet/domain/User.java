eafd4e645f8a5d15e19105e3619c21f1e2edb986
FG <gomezfv95@gmail.com>
2024-05-23 12:09:33 -0300

    added UserRepository , UserService

--- src/main/java/AlkemyWallet/AlkemyWallet/domain/User.java
+++ src/main/java/AlkemyWallet/AlkemyWallet/domain/User.java
@@ -1,17 +1,18 @@
 package AlkemyWallet.AlkemyWallet.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import org.antlr.v4.runtime.misc.NotNull;
 
 import java.time.LocalDateTime;
 
 @Data
 @Entity
-@Table(name="Users")
+@Table(name="users")
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
@@ -21,23 +22,36 @@ public class User {
     private Long id;
 
     @Column(nullable = false)
+    @NotNull
     private String firstName;
-    @Column(nullable = false)
 
+    @Column(nullable = false)
+    @NotNull
     private String lastName;
+
     @Column(nullable = false)
+    @NotNull
+    @Email
     private String email;
+
     @Column(nullable = false)
+    @NotNull
     private String password;
+
     @Column(nullable = false)
+    @NotNull
     private LocalDateTime creationDate;
+
     @Column(nullable = false)
+    @NotNull
     private LocalDateTime updateDate;
+
     @Column(nullable = false)
+    @NotNull
     private boolean softDelete;
 
-
-    //private roleId RoleId;
-
+   // @ManyToOne
+   // @JoinColumn(name='role_id', nullable = false)
+    //private Role roleid;
 
 }
--- src/main/java/AlkemyWallet/AlkemyWallet/repositories/UserRepository.java
+++ src/main/java/AlkemyWallet/AlkemyWallet/repositories/UserRepository.java
@@ -1,4 +1,13 @@
 package AlkemyWallet.AlkemyWallet.repositories;
 
-public class UserRepository {
+import AlkemyWallet.AlkemyWallet.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+   Optional<User> findByEmail(String email);
 }
