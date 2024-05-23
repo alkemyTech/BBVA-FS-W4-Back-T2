a371acbe51d556399f40e66b724a75e9b3d6727f
Mica Gobbi <mgobbi@frba.utn.edu.ar>
2024-05-23 16:07:01 -0300

    Implemento logica JWT

--- src/main/java/AlkemyWallet/AlkemyWallet/domain/User.java
+++ src/main/java/AlkemyWallet/AlkemyWallet/domain/User.java
@@ -7,15 +7,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Data
 @Entity
 @Table(name="users")
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -50,7 +53,37 @@ public class User {
     @NotNull
     private boolean softDelete;
 
-   // @ManyToOne
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    // @ManyToOne
    // @JoinColumn(name='role_id', nullable = false)
     //private Role roleid;
 
---
+++ src/main/java/AlkemyWallet/AlkemyWallet/services/JwtService.java
@@ -0,0 +1,42 @@
+package AlkemyWallet.AlkemyWallet.services;
+import AlkemyWallet.AlkemyWallet.security.config.JwtConfig;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+
+@Service
+@AllArgsConstructor
+public class JwtService {
+
+    private JwtConfig jwtConfig;
+
+    public String createToken(String userId, int minutesValid) {
+        var algorithm = Algorithm.HMAC256(jwtConfig.getSecret());
+        return JWT.create()
+                .withIssuer(jwtConfig.getIssuer())
+                .withClaim("userId", userId)
+                .withExpiresAt(new Date(System.currentTimeMillis() + minutesValid * 60 * 1000))
+                .sign(algorithm);
+    }
+
+
+    public DecodedJWT verifyToken(String token) throws JWTVerificationException {
+        var algorithm = Algorithm.HMAC256(jwtConfig.getSecret());
+        var verifier = JWT.require(algorithm)
+                .withIssuer(jwtConfig.getIssuer())
+                .build();
+        return verifier.verify(token);
+    }
+
+    public String extractUserId(String token) {
+        var jwt = JWT.decode(token);
+        return jwt.getClaim("userId").asString();
+    }
+
+}
