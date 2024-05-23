87e509fafa19e1c0186af49e8373cac161326144
FG <gomezfv95@gmail.com>
2024-05-23 11:28:33 -0300

    added jpa dependencies to users

--- src/main/java/AlkemyWallet/AlkemyWallet/domain/User.java
+++ src/main/java/AlkemyWallet/AlkemyWallet/domain/User.java
@@ -19,6 +19,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String firstName;
     @Column(nullable = false)
