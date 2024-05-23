fbc7b9dc8e47a55407edff4edc142dab76feda1e
FG <gomezfv95@gmail.com>
2024-05-23 10:59:28 -0300

    added User entity variables

--- src/main/java/AlkemyWallet/AlkemyWallet/AlkemyWalletApplication.java
+++ src/main/java/AlkemyWallet/AlkemyWallet/AlkemyWalletApplication.java
@@ -3,11 +3,18 @@ package AlkemyWallet.AlkemyWallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 @SpringBootApplication
 public class AlkemyWalletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlkemyWalletApplication.class, args);
-	}
 
+
+
+}
 }
--- src/main/java/AlkemyWallet/AlkemyWallet/domain/User.java
+++ src/main/java/AlkemyWallet/AlkemyWallet/domain/User.java
@@ -1,4 +1,42 @@
 package AlkemyWallet.AlkemyWallet.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="Users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+
+    private String lastName;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+    @Column(nullable = false)
+    private LocalDateTime updateDate;
+    @Column(nullable = false)
+    private boolean softDelete;
+
+
+    //private roleId RoleId;
+
+
 }
