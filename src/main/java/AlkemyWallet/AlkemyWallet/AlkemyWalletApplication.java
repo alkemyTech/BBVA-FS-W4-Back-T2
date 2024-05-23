d31c8f21da318e0ddfe2980b859dd5f9bf8b486d
FG <gomezfv95@gmail.com>
2024-05-23 11:00:42 -0300

    added conection to data base in main app

--- src/main/java/AlkemyWallet/AlkemyWallet/AlkemyWalletApplication.java
+++ src/main/java/AlkemyWallet/AlkemyWallet/AlkemyWalletApplication.java
@@ -3,11 +3,29 @@ package AlkemyWallet.AlkemyWallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class AlkemyWalletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlkemyWalletApplication.class, args);
+
+
+		// Conexion base de datos
+		String url = "jdbc:mysql://localhost:3306/alkemywallet";
+		String user = "root";
+		String password = "gatosmonteses";
+		try{
+			Connection conexion = DriverManager.getConnection(url,user,password);
+			System.out.printf("Conexion establecida con exito\n");
+			conexion.close();
+		} catch (
+				SQLException e) {
+			System.out.println("Error al conectar con la base de datos");
+		}
 	}
 
 }
