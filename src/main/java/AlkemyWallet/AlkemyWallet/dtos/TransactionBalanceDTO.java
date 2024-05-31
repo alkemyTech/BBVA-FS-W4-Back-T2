47656ec8f918cc79fa9b6eee9efddced1d57b515
FG <gomezfv95@gmail.com>
2024-05-31 16:33:39 -0300

    deleted unused imports in transactionbalancedto

--- src/main/java/AlkemyWallet/AlkemyWallet/dtos/TransactionBalanceDTO.java
+++ src/main/java/AlkemyWallet/AlkemyWallet/dtos/TransactionBalanceDTO.java
@@ -1,16 +1,12 @@
 package AlkemyWallet.AlkemyWallet.dtos;
 
 import AlkemyWallet.AlkemyWallet.enums.TransactionEnum;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
