888d810ed3137fdb05fa6bf18817284ae4862a42
Joan <joanes.catalan@gmail.com>
2024-06-08 12:09:33 -0300

    Modifico AccountsSeeder porque no estaba actualizando balance en las cuentas

--- src/main/java/AlkemyWallet/AlkemyWallet/config/AppConfiguration.java
+++ src/main/java/AlkemyWallet/AlkemyWallet/config/AppConfiguration.java
@@ -25,7 +25,7 @@ public class AppConfiguration {
         return args -> {
             userSeeder.seed();
             accountsSeeder.seedAccounts();
-            transactionSeeder.run();
+            //transactionSeeder.run();
         };
     }
 }
