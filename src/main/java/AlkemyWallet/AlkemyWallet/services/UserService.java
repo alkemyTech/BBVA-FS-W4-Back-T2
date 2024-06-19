ae4ab50d398a69b5069982ee799d4a7986775c56
micangob <85760771+micangob@users.noreply.github.com>
2024-06-19 16:21:03 -0300

    Update UserService.java

--- src/main/java/AlkemyWallet/AlkemyWallet/services/UserService.java
+++ src/main/java/AlkemyWallet/AlkemyWallet/services/UserService.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 import java.time.LocalDateTime;
